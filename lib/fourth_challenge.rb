a859c3abb2a3982c4418030d4ddfc407bea0fa89
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-27 06:20:05 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,10 +24,4 @@ def fourth_challenge
       :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude => "worried"}]
    }
   }
-
-  
-end
-
-  
-
 end
